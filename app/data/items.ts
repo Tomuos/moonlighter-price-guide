6d4f97cf749cea229eb8af3ae2af345f6a91c944
Tomuos <tomulvlup@gmail.com>
2025-08-30 22:22:31 +0100

    updated info and added images

--- app/data/items.ts
+++ app/data/items.ts
@@ -3,12 +3,13 @@ import type { DungeonId, DungeonItem } from "../../constants/types"; // ✅ fix 
 import { GOLEM_ITEMS } from "./golemItems";
 import { FOREST_ITEMS } from "./forestItems";
 import { DESERT_ITEMS } from "./desertItems";
+import { TECH_ITEMS } from "./techItems";
 
 export const ALL_ITEMS: Record<DungeonId, DungeonItem[]> = {
   golem: GOLEM_ITEMS,
   forest: FOREST_ITEMS,
   desert: DESERT_ITEMS,
-  tech: [TECH_ITEMS],
+  tech: TECH_ITEMS,
   wanderer: [],
 };
 
