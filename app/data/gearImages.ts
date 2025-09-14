99da7e5bd4fada36b4d1a51127b2502eab74db94
Tomuos <tomulvlup@gmail.com>
2025-09-14 18:07:26 +0100

    images have been added and assigned through gearimages

--- app/data/gearImages.ts
+++ app/data/gearImages.ts
@@ -11,12 +11,19 @@ export const gearImages: Record<string, ImageSourcePropType> = {
 
 
 "barrier-amulet": require("../../assets/images/gear/barrier-amulet.png"),
+"blaze-big-sword": require("../../assets/images/gear/blaze-big-sword.png"),
 "black-cat-amulet": require("../../assets/images/gear/black-cat-amulet.png"),
+"broom-spear": require("../../assets/images/gear/broom-spear.png"),
+"buster-big-sword": require("../../assets/images/gear/buster-big-sword.png"),
 
 
+"captain-gloves": require("../../assets/images/gear/captain-gloves.png"),
 "carnivorous-mutae-amulet": require("../../assets/images/gear/carnivorous-mutae-amulet.png"),
+"catapult-bow": require("../../assets/images/gear/catapult-bow.png"),
 "charged-attack-amulet": require("../../assets/images/gear/charged-attack-amulet.png"),
+"commander-short-sword": require("../../assets/images/gear/commander-short-sword.png"),
 "confu-elemental-amulet": require("../../assets/images/gear/confu-elemental-amulet.png"),
+
 
 
 "electric-elemental-amulet": require("../../assets/images/gear/electric-elemental-amulet.png"),
@@ -26,6 +33,8 @@ export const gearImages: Record<string, ImageSourcePropType> = {
 "emergency-health-amulet": require("../../assets/images/gear/emergency-health-amulet.png"),
 "emergency-speed-amulet": require("../../assets/images/gear/emergency-speed-amulet.png"),
 "energy-flux-amulet": require("../../assets/images/gear/energy-flux-amulet.png"),
+"ether-sable": require("../../assets/images/gear/ether-sable.png"),
+"exter-bow": require("../../assets/images/gear/exeter-bow.png"),
 
 
 // "": require("../../assets/images/gear"), <-- when you need to add a new line
@@ -33,42 +42,93 @@ export const gearImages: Record<string, ImageSourcePropType> = {
 
 "familiar-amulet": require("../../assets/images/gear/familiar-amulet.png"),
 "fenix-amulet": require("../../assets/images/gear/fenix-amulet.png"),
+"fighter-gloves": require("../../assets/images/gear/fighter-gloves.png"),
+"fighter-spear": require("../../assets/images/gear/fighter-spear.png"),
 "fire-elemental-amulet": require("../../assets/images/gear/fire-elemental-amulet.png"),
 "fire-puddle-amulet": require("../../assets/images/gear/fire-puddle-amulet.png"),
+"flame-gloves": require("../../assets/images/gear/flame-gloves.png"),
+"flamethrower-bow": require("../../assets/images/gear/flamethrower-bow.png"),
+"forest-spirit-gloves": require("../../assets/images/gear/forest-spirit-gloves.png"),
+"fusion-big-sword": require("../../assets/images/gear/fusion-big-sword.png"),
 
 
+"golem-drill-spear": require("../../assets/images/gear/golem-drill-spear.png"),
 "golem-king-amulet": require("../../assets/images/gear/golem-king-amulet.png"),
 "golem-turret-familiar-amulet": require("../../assets/images/gear/golem-turret-familiar-amulet.png"),
 "graaf-generator-familiar-amulet": require("../../assets/images/gear/graaf-generator-familiar-amulet.png"),
 
 
+"hell-spear": require("../../assets/images/gear/hell-spear.png"),
 "hero-ring": require("../../assets/images/gear/hero-ring.png"),
+"heros-bow": require("../../assets/images/gear/heros-bow.png"),
+"heros-sword": require("../../assets/images/gear/heros-sword.png"),
+"hunter-bow": require("../../assets/images/gear/hunter-bow.png"),
 
 
 "insta-kill-amulet": require("../../assets/images/gear/insta-kill-amulet.png"),
 
 
+"king-short-sword": require("../../assets/images/gear/king-short-sword.png"),
+"knight-short-sword": require("../../assets/images/gear/knight-short-sword.png"),
+
+
 "life-drain-amulet": require("../../assets/images/gear/life-drain-amulet.png"),
+"lightning-bow": require("../../assets/images/gear/lightning-bow.png"),
+"lightning-spear": require("../../assets/images/gear/lightning-spear.png"),
 "luck-amulet": require("../../assets/images/gear/luck-amulet.png"),
 
 
+"master-fists": require("../../assets/images/gear/master-fists.png"),
 "mother-golem-familiar-amulet": require("../../assets/images/gear/mother-golem-familiar-amulet.png"),
+"monkey-spear": require("../../assets/images/gear/monkey-spear.png"),
 
 
 "naja-amulet": require("../../assets/images/gear/naja-amulet.png"),
+"natural-bow": require("../../assets/images/gear/natural-bow.png"),
 
 
+"poison-bow": require("../../assets/images/gear/poison-bow.png"),
 "poison-elemental-amulet": require("../../assets/images/gear/poison-elemental-amulet.png"),
 "poison-puddle-amulet": require("../../assets/images/gear/poison-puddle-amulet.png"),
 
 
+"ragnarok-big-sword": require("../../assets/images/gear/ragnarok-big-sword.png"),
+"reborn-short-sword": require("../../assets/images/gear/reborn-short-sword.png"),
+"rock-big-sword": require("../../assets/images/gear/rock-big-sword.png"),
+"rough-gloves": require("../../assets/images/gear/rough-gloves.png"),
+"runic-bow": require("../../assets/images/gear/runic-bow.png"),
+"rusty-short-sword": require("../../assets/images/gear/rusty-short-sword.png"),
+
+
+
+"soldier-bow": require("../../assets/images/gear/soldier-bow.png"),
+"soldier-short-sword": require("../../assets/images/gear/soldier-short-sword.png"),
 "spiked-roll-amulet": require("../../assets/images/gear/spiked-roll-amulet.png"),
+"star-platinum-gloves": require("../../assets/images/gear/star-platinum-gloves.png"),
+"storm-big-sword": require("../../assets/images/gear/storm-big-sword.png"),
 
 
+"thunder-gloves": require("../../assets/images/gear/thunder-gloves.png"),
+"toxic-big-sword": require("../../assets/images/gear/toxic-big-sword.png"),
+"training-big-sword": require("../../assets/images/gear/training-big-sword.png"),
+"training-bow": require("../../assets/images/gear/training-bow.png"),
+"training-gloves": require("../../assets/images/gear/training-gloves.png"),
+"training-short-sword": require("../../assets/images/gear/training-short-sword.png"),
+"training-spear": require("../../assets/images/gear/training-spear.png"),
+
+
+"valkyrie-spear": require("../../assets/images/gear/valkyrie-spear.png"),
 "vampire-amulet": require("../../assets/images/gear/vampire-amulet.png"),
+"vampire-short-sword": require("../../assets/images/gear/vampire-short-sword.png"),
+"venom-short-sword": require("../../assets/images/gear/venom-short-sword.png"),
+"venom-sting-spear": require("../../assets/images/gear/venom-sting-spear.png"),
+"venom-twins-gloves": require("../../assets/images/gear/venom-twins-gloves.png"),
+"vulcan-big-sword": require("../../assets/images/gear/vulcan-big-sword.png"),
 
 
 "wanderer-amulet": require("../../assets/images/gear/wanderer-amulet.png"),
+"warrior-spear": require("../../assets/images/gear/warrior-spear.png"),
+"wild-big-sword": require("../../assets/images/gear/wild-big-sword.png"),
 "wind-tree-sprout-familiar-amulet": require("../../assets/images/gear/wind-tree-sprout-familiar-amulet.png"),
 
 
